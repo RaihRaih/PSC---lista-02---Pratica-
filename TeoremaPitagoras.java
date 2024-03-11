46bf0acf43b8ce02d77af6d57436047d95d510ee
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-03-11 20:24:23 -0300

    Add files via upload

---
+++ TeoremaPitagoras.java
@@ -0,0 +1,27 @@
+/*Raissa Rodrigues - Sistemas de Informação
+ *2. Escreva um programa que calcule o teorema de Pitágoras, representado pela expressão: c =a^2+b^2 */ 
+
+import java.util.Scanner;
+
+public class TeoremaPitago {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double a, b, c, raiz;
+
+        System.out.println("Digite o valor de a: ");
+        a = scanner.nextDouble();
+        
+        System.out.println("Digite o valor de b: ");
+        b = scanner.nextDouble();
+
+        c = (a*a) + (b*b);
+
+        raiz =  Math.sqrt(c);
+
+        System.out.println("Valor de C: "+ raiz);
+
+
+    }
+}
